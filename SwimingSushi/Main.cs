668a0f651c39dd63d86acaeeb73b7ddf6e203837
Lachlan Leone <starfluxgames@gmail.com>
2023-02-21 11:52:52 +1100

    v0.1.9

--- SwimingSushi/Main.cs
+++ SwimingSushi/Main.cs
@@ -16,7 +16,7 @@ namespace SwimingSushi
 {
 	public class Main : BaseMod
 	{
-		public Main() : base("swimingsushi", "Swiming Sushi", "StarFluxGames", "0.1.8", "1.1.4", Assembly.GetExecutingAssembly()) { }
+		public Main() : base("swimingsushi", "Swimming Sushi", "StarFluxGames", "0.1.9", "1.1.4", Assembly.GetExecutingAssembly()) { }
 
 		public static AssetBundle bundle;
 
